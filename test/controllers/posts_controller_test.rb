a8beee073d73f33277392cc28884c9814a1227da
Valeriy Kassenbaev <vkassenbaev@gmail.com>
2015-02-06 23:47:32 +0200

    tests for posts controller added

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+  test "geting posts index" do
+    get :index
+    assert_response :success
+    assert_select "div.post", { :count => 3 }
+  end
+  
+  test "posts show should redirect to login page if not logged in" do
+    get :show, id: posts(:post_1).id
+    assert_redirected_to login_path
+  end
+  
+  test "posts show should display the post" do
+    log_in_as(users(:user))
+    get :show, id: posts(:post_1).id
+    assert_response :success
+  end
 end
