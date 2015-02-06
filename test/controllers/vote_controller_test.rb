dc8a820d6fe7fdccf00d08394c0edb5fbc5f135a
Valeriy Kassenbaev <vkassenbaev@gmail.com>
2015-02-06 07:48:11 +0200

    unittests failures fixed

--- test/controllers/vote_controller_test.rb
+++ test/controllers/vote_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class VoteControllerTest < ActionController::TestCase
-  test "should get up" do
-    get :up
-    assert_response :success
-  end
-
-  test "should get down" do
-    get :down
-    assert_response :success
-  end
-
 end
