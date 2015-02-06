17a0edb73e8de0c6ca7fe0f19f9e600ea37393af
Valeriy Kassenbaev <vkassenbaev@gmail.com>
2015-02-06 23:44:55 +0200

    test_helper.rb: is_logged_in and log_in_as helpers added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,25 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :users
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || "password"
+    if integration_test?
+      post login_path, session: { name: user.name,
+                                  password: password }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
